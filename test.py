1e4d7be878c3b3512a93f7b8e1aa1ed5b95b9545
jxwleong <jason.xie.wei.leong@gmail.com>
2021-12-19 15:55:06 +0800

    Added for pseudocode for testing register page. However, the captcha we're thinking about select the image manually first..

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import time
+
 # Assume the chromedriver.exe haveing the same directory as this file..
-driver = webdriver.Chrome("chromedriver.exe")
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
+driver.get(url="https://demo.opencart.com/")
+print("get")
+time.sleep(10)
+
+
+# Register
+# url: https://www.opencart.com/index.php?route=account/register
+username = driver.find_element_by_name("username")
+firstname = driver.find_element_by_name("firstname")
+lastname = driver.find_element_by_name("lastname")
+email = driver.find_element_by_name("email")
+# Select country dropdown box
+# https://www.guru99.com/select-option-dropdown-selenium-webdriver.html
+country = driver.find_element_by_name("country_id")
+password = driver.find_element_by_name("password")
+
+
+
+print("close")
+driver.close()
